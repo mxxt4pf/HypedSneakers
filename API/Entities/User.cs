33c7cac1302212f69e394c09583fd791605770a6
NisargPatel <56943727+nisargpatidar@users.noreply.github.com>
2023-07-28 01:08:28 -0600

    Mainly the ContactUs and AboutUs Page

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -8,6 +8,11 @@ namespace API.Entities
 {
     public class User : IdentityUser<int>
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public UserAddress Address { get; set; }
+        public string EmailID { get; set; }
+        public string Password { get; set; }
     }
 }
